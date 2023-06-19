5ba3678c5f1c41f1d0e89d33bfd6924aa0f5b615
Porkalaikarthika <porkalaikarthika@gmail.com>
2023-06-19 22:48:11 +0530

    fixed linter

--- menu.js
+++ menu.js
@@ -1,24 +1,24 @@
 const menuIcon = document.getElementById('menu-icon');
 const closeIcon = document.getElementById('close-icon');
 const mobileMenu = document.getElementById('mobile-menu');
-const menuItem = mobileMenu.getElementsByClassName('menuItems')
+const menuItem = mobileMenu.getElementsByClassName('menuItems');
 
 function toggleMenu() {
-    mobileMenu.classList.toggle('visible');
-    closeIcon.classList.toggle('visible');
-    menuIcon.classList.toggle('hidden');
+  mobileMenu.classList.toggle('visible');
+  closeIcon.classList.toggle('visible');
+  menuIcon.classList.toggle('hidden');
 }
 
-for (let i = 0; i < menuItem.length; i++) {
-    menuItem[i].addEventListener('click', function () {
-        toggleMenu();
-    })
-}
-
-menuIcon.addEventListener('click', function () {
+for (let i = 0; i < menuItem.length; i++ ) {
+  menuItem[i].addEventListener('click', function() {
     toggleMenu();
+  });
+}
+
+menuIcon.addEventListener('click', function() {
+  toggleMenu();
 });
 
-closeIcon.addEventListener('click', function () {
-    toggleMenu();
+closeIcon.addEventListener('click', function() {
+  toggleMenu();
 });
