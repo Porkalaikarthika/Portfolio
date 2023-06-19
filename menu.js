ff0191f62ec2a442a2c0d931850ea58f4545e814
Porkalaikarthika <porkalaikarthika@gmail.com>
2023-06-19 21:24:47 +0530

    add eventlisteners to menu items

--- menu.js
+++ menu.js
@@ -1,10 +1,19 @@
 const menuIcon = document.getElementById('menu-icon');
 const closeIcon = document.getElementById('close-icon');
 const mobileMenu = document.getElementById('mobile-menu');
+const menuItem = mobileMenu.getElementsByClassName('menuItems')
 
 function toggleMenu() {
   mobileMenu.classList.toggle('visible');
+  closeIcon.classList.toggle('visible');
   menuIcon.classList.toggle('hidden');
+}
+
+for(let i=0;i<menuItem.length;i++)
+{
+    menuItem[i].addEventListener('click',function(){
+        toggleMenu();
+    })
 }
 
 menuIcon.addEventListener('click', function() {
