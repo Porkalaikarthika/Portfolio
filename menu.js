74358ef2a0c870e3155ef0711dc2e363bdec2e87
Porkalaikarthika <porkalaikarthika@gmail.com>
2023-06-20 18:57:37 +0530

    Created dynamic html

--- menu.js
+++ menu.js
@@ -22,3 +22,47 @@ menuIcon.addEventListener('click', () => {
 closeIcon.addEventListener('click', () => {
   toggleMenu();
 });
+
+const Projects ={
+  card1: {
+    name: 'Project 1',
+    description: 'This is the first project',
+    image: 'project1.jpg',
+    technologies: 'HTML, CSS, JavaScript',
+    liveLink: 'https://example.com/project1',
+    sourceLink: 'https://github.com/project1'
+  },
+  card2: {
+    name: 'Project 2',
+    description: 'This is the second project',
+    image: 'project2.jpg',
+    technologies: 'React, Node.js',
+    liveLink: 'https://example.com/project2',
+    sourceLink: 'https://github.com/project2'
+  },
+  card3: {
+    name: 'Project 2',
+    description: 'This is the second project',
+    image: 'project2.jpg',
+    technologies: 'React, Node.js',
+    liveLink: 'https://example.com/project2',
+    sourceLink: 'https://github.com/project2'
+  },
+  card4: {
+    name: 'Project 2',
+    description: 'This is the second project',
+    image: 'project2.jpg',
+    technologies: 'React, Node.js',
+    liveLink: 'https://example.com/project2',
+    sourceLink: 'https://github.com/project2'
+  }
+};
+
+const projectsContainer = document.getElementById('projects');
+
+for (const key in object) {
+  if (Object.hasOwnProperty.call(object, key)) {
+    const element = object[key];
+    
+  }
+}
