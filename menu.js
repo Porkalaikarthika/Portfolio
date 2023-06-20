57b243c8c9b3c868b5d522f11b1a2e52dd1f40c7
Porkalaikarthika <porkalaikarthika@gmail.com>
2023-06-20 21:14:45 +0530

    Added section details

--- menu.js
+++ menu.js
@@ -22,3 +22,23 @@ menuIcon.addEventListener('click', () => {
 closeIcon.addEventListener('click', () => {
   toggleMenu();
 });
+
+const projects = [
+  {
+    name: 'Project 1',
+    description: 'This is the first project',
+    image: 'project1.jpg',
+    technologies: 'HTML, CSS, JavaScript',
+    liveLink: 'https://example.com/project1',
+    sourceLink: 'https://github.com/project1'
+  },
+  {
+    name: 'Project 2',
+    description: 'This is the second project',
+    image: 'project2.jpg',
+    technologies: 'React, Node.js',
+    liveLink: 'https://example.com/project2',
+    sourceLink: 'https://github.com/project2'
+  },
+  // Add more projects as needed
+];
