b541c2908b82c1317a06175458a500379a4b9f98
Porkalaikarthika <porkalaikarthika@gmail.com>
2023-06-21 16:17:35 +0530

    Added error message

--- menu.js
+++ menu.js
@@ -24,27 +24,24 @@ closeIcon.addEventListener('click', () => {
 });
 
 //----------------contact form validation-----------------------
-// Function to handle form submission
-function handleSubmit(event) {
-  event.preventDefault(); // Prevent form from being submitted
 
-  // Get the email input value and convert it to lower case
+function handleSubmit(event) {
+  event.preventDefault();
+
   const emailInput = document.getElementById("email");
   const email = emailInput.value.toLowerCase();
 
-  // Check if the email is in lower case
   if (email === emailInput.value) {
-    // Validation is OK, submit the form
+
     document.getElementById("myForm").submit();
   } else {
-    // Validation failed, show error message
-    const errorMessage = "Email must be in lower case.";
+    const errorMessage = "Email must be in lower case, Form is not submitted";
     const errorElement = document.getElementById("error");
     errorElement.textContent = errorMessage;
+    
   }
 }
 
-// Add form submission event listener
 const form = document.getElementById("myForm");
 form.addEventListener("submit", handleSubmit);
 
