465afbde39d3e6b8b0dc73edb3a94272e82080b2
Porkalaikarthika <porkalaikarthika@gmail.com>
2023-06-21 16:04:20 +0530

    Added form validation for email

--- menu.js
+++ menu.js
@@ -22,3 +22,30 @@ menuIcon.addEventListener('click', () => {
 closeIcon.addEventListener('click', () => {
   toggleMenu();
 });
+
+//----------------contact form validation-----------------------
+// Function to handle form submission
+function handleSubmit(event) {
+  event.preventDefault(); // Prevent form from being submitted
+
+  // Get the email input value and convert it to lower case
+  const emailInput = document.getElementById("email");
+  const email = emailInput.value.toLowerCase();
+
+  // Check if the email is in lower case
+  if (email === emailInput.value) {
+    // Validation is OK, submit the form
+    document.getElementById("myForm").submit();
+  } else {
+    // Validation failed, show error message
+    const errorMessage = "Email must be in lower case.";
+    const errorElement = document.getElementById("error");
+    errorElement.textContent = errorMessage;
+  }
+}
+
+// Add form submission event listener
+const form = document.getElementById("myForm");
+form.addEventListener("submit", handleSubmit);
+
+
