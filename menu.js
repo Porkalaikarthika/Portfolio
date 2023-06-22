588ed38c4d5df1b033961eb9ad9a0ec65013a94e
Porkalaikarthika <porkalaikarthika@gmail.com>
2023-06-22 17:03:58 +0530

    made changes as per review

--- menu.js
+++ menu.js
@@ -36,6 +36,8 @@ const projects = [
     year: '2015',
     liveLink: '#',
     sourceLink: '#',
+    imgSrcMobile: 'desktop-img/Snap1.png',
+    imgSrcDesktop: 'desktop-img/Snap1.png',
   },
   {
     name: 'Multi-Post Stories',
@@ -47,6 +49,8 @@ const projects = [
     year: '2015',
     liveLink: '#',
     sourceLink: '#',
+    imgSrcMobile: 'desktop-img/Snap2.png',
+    imgSrcDesktop: 'desktop-img/Snap2.png',
   },
   {
     name: 'Facebook 360',
@@ -58,6 +62,8 @@ const projects = [
     year: '2015',
     liveLink: '#',
     sourceLink: '#',
+    imgSrcMobile: 'desktop-img/Snap3.png',
+    imgSrcDesktop: 'desktop-img/Snap3.png',
   },
   {
     name: 'Uber Navigation',
@@ -69,18 +75,20 @@ const projects = [
     year: '2018',
     liveLink: '#',
     sourceLink: '#',
+    imgSrcMobile: 'desktop-img/Snap4.png',
+    imgSrcDesktop: 'desktop-img/Snap4.png',
   },
 ];
 document.getElementById('close-popup').addEventListener('click', () => {
-  modal.classList.toggle('show');
+  modal.classList.remove('show');
 });
 
 function ShowModal(index) {
   document.getElementById('popupName').textContent = projects[index].name;
   document.getElementById('popupImage').setAttribute('src', projects[index].image);
   document.getElementById('popupDescription').textContent = projects[index].description;
-  document.getElementById('popupLive').setAttribute('href', projects[index].liveLink);
-  document.getElementById('popupSource').setAttribute('href', projects[index].sourceLink);
+  // document.getElementById('popupLive').setAttribute('href', projects[index].liveLink);
+  // document.getElementById('popupSource').setAttribute('href', projects[index].sourceLink);
   document.getElementById('client').textContent = projects[index].client;
   document.getElementById('role').textContent = projects[index].role;
   document.getElementById('year').textContent = projects[index].year;
@@ -94,25 +102,108 @@ function ShowModal(index) {
     li.appendChild(button);
     techList.appendChild(li);
   });
+
+  const liveLink = projects[index].liveLink;
+  const sourceLink = projects[index].sourceLink;
+
+  const liveButtonContainer = document.querySelector('.liveButton');
+  liveButtonContainer.innerHTML = '';
+
+  const liveButton = document.createElement('a');
+  liveButton.href = liveLink;
+  liveButton.classList.add('buttonlrg');
+  liveButton.innerHTML = `
+    See Live
+    <img src="img/Icon.png" alt="See Live Icon">
+  `;
+
+  const sourceButton = document.createElement('a');
+  sourceButton.href = sourceLink;
+  sourceButton.classList.add('buttonlrg');
+  sourceButton.innerHTML = `
+    See Source
+    <img src="img/live.png" alt="See Source Icon">
+  `;
+
+  liveButtonContainer.appendChild(liveButton);
+  liveButtonContainer.appendChild(sourceButton);
 }
 
-const showButton1 = document.getElementById('seeProject1');
-showButton1.addEventListener('click', () => {
-  modal.classList.toggle('show');
-  ShowModal(0);
-});
-const showButton2 = document.getElementById('seeProject2');
-showButton2.addEventListener('click', () => {
-  modal.classList.toggle('show');
-  ShowModal(1);
-});
-const showButton3 = document.getElementById('seeProject3');
-showButton3.addEventListener('click', () => {
-  modal.classList.toggle('show');
-  ShowModal(2);
-});
-const showButton4 = document.getElementById('seeProject4');
-showButton4.addEventListener('click', () => {
-  modal.classList.toggle('show');
-  ShowModal(3);
+const projectsContainer = document.getElementById('projects-container');
+
+projects.forEach((project, index) => {
+
+  const projectSection = document.createElement('section');
+  projectSection.classList.add('card');
+  if (index==1||3)
+  {
+    projectSection.classList.add('rev');
+  }
+
+  const imgMobile = document.createElement('img');
+  imgMobile.classList.add('mimg', 'projectImg');
+  imgMobile.src = project.imgSrcMobile;
+  projectSection.appendChild(imgMobile);
+
+  const imgDesktop = document.createElement('img');
+  imgDesktop.classList.add('dimg', 'projectImg');
+  imgDesktop.src = project.imgSrcDesktop;
+  projectSection.appendChild(imgDesktop);
+
+  const descriptionDiv = document.createElement('div');
+  descriptionDiv.classList.add('description');
+  projectSection.appendChild(descriptionDiv);
+
+  const titleHeading = document.createElement('h4');
+  titleHeading.classList.add('heading');
+  titleHeading.textContent = project.name;
+  descriptionDiv.appendChild(titleHeading);
+
+  const ul = document.createElement('ul');
+  ul.classList.add('list');
+  descriptionDiv.appendChild(ul);
+
+  const canopyLi = document.createElement('li');
+  canopyLi.classList.add('bullet');
+  canopyLi.textContent = `${project.client}`;
+  ul.appendChild(canopyLi);
+
+  const roleLi = document.createElement('li');
+  roleLi.classList.add('bullet');
+  roleLi.textContent = `${project.role}`;
+  ul.appendChild(roleLi);
+
+  const yearLi = document.createElement('li');
+  yearLi.classList.add('bullet');
+  yearLi.textContent = `${project.year}`;
+  ul.appendChild(yearLi);
+
+  const descriptionParagraph = document.createElement('p');
+  descriptionParagraph.textContent = project.description;
+  descriptionDiv.appendChild(descriptionParagraph);
+
+  const technologiesList = document.createElement('ul');
+  technologiesList.classList.add('buttonList');
+  project.technologies.forEach(technology => {
+    const technologyLi = document.createElement('li');
+    const technologyButton = document.createElement('button');
+    technologyButton.classList.add('smallbtn');
+    technologyButton.textContent = technology;
+    technologyLi.appendChild(technologyButton);
+    technologiesList.appendChild(technologyLi);
+  });
+  descriptionDiv.appendChild(technologiesList);
+
+  const seeProjectButton = document.createElement('button');
+  seeProjectButton.classList.add('buttonlrg', 'details-button');
+  seeProjectButton.id = `seeProject${index + 1}`;
+  seeProjectButton.textContent = 'See project';
+  seeProjectButton.addEventListener('click', () => {
+    modal.classList.add('show');
+    ShowModal(index);
+  });
+  descriptionDiv.appendChild(seeProjectButton);
+
+  projectsContainer.appendChild(projectSection);
+
 });
